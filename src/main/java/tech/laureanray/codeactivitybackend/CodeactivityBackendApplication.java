10b3fc1e937075697218127b3f6637009c51bdc3
laureanray <laureanraybahala@gmail.com>
2020-04-20 15:11:43 +0800

    initialized spring project

---
+++ src/main/java/tech/laureanray/codeactivitybackend/CodeactivityBackendApplication.java
@@ -0,0 +1,13 @@
+package tech.laureanray.codeactivitybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeactivityBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeactivityBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/laureanray/codeactivitybackend/CodeactivityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.laureanray.codeactivitybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeactivityBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
